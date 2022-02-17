5ca7e9de418d430de73203e69ed41b713a7840c7
Alexander <48823227+Catsville60@users.noreply.github.com>
2022-02-17 23:17:02 +0700

    do all TODO's and features!!! PIDORBOT is ALIVE!!!!

--- bot.py
+++ bot.py
@@ -2,94 +2,87 @@
 """Bot just for fun and trash
 and of course choose who's the faggot of the day!"""
 
-
-
 import random
 import telebot
 import json
 import traceback
 import datetime
 
-#TODO Hide the token
-#TODO Change stickers
-
 # Token
-with open("token.txt") as f:
-    TOKEN = f.read().strip()
-bott = telebot.TeleBot(f'{TOKEN}')
+with open("token.txt") as token_file:
+    token = token_file.read().strip()
+bott = telebot.TeleBot(f'{token}')
 bott.remove_webhook()
 
 # Just say Hi!
 @bott.message_handler(commands=['hi_pidar'])
 def hi_pidar(message):
     bott.send_message(message.chat.id, "Привет, пидар!")
-    bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+    bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED9WxiDllenmvORyM3NDb_duPmI7E6rgACtwMAAgk7OxO9QpPr5PZ-ciME')
 
 # Registration new pidor
 @bott.message_handler(commands=['reg_pidar'])
 def reg_pidar(message):
     try:
-        Usernic = message.from_user.username
-        Username = message.from_user.first_name
-        User_chat_id = message.from_user.id
-        Score = 0
+        usernick = message.from_user.username
+        username = message.from_user.first_name
+        user_chat_id = message.from_user.id
+        score = 0
         trigger = 0
 
         with open('users.json', 'r') as users_file_r:
-            Users_list = json.load(users_file_r)
-            Users_list_unparse = Users_list['Users']
-            Users_ids = Users_list['id']
+            users_list = json.load(users_file_r)
+            users_list_unparse = users_list['Users']
+            users_ids = users_list['id']
 
-        for User_id in Users_ids:
-            if User_chat_id == User_id:
+        for user_id in users_ids:
+            if user_chat_id == user_id:
                 trigger = 1
                 break
 
-        for User in Users_list_unparse:
-            if Usernic == User:
+        for user in users_list_unparse:
+            if usernick == user:
                 trigger = 1
                 break
 
-            elif Username == User:
+            elif username == user:
                 trigger = 1
                 break
 
         if trigger == 1:
-            if Usernic is not None:
-                bott.send_message(message.chat.id, f'@{Usernic} ты уже в Пидорленде!')
-                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+            if usernick is not None:
+                bott.send_message(message.chat.id, f'@{usernick} ты уже в Пидорленде!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBTEFgn_j6RP8Ec5CyJ3cqmR6oUNFLqwACsgMAAgk7OxNQtIaPmAABfG4fBA')
 
             else:
-                bott.send_message(message.chat.id, f'{Username} ты уже в Пидорленде!')
-                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+                bott.send_message(message.chat.id, f'{username} ты уже в Пидорленде!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBTEFgn_j6RP8Ec5CyJ3cqmR6oUNFLqwACsgMAAgk7OxNQtIaPmAABfG4fBA')
 
         else:
-            if Usernic is not None:
-                bott.send_message(message.chat.id, f'Добро пожаловать в Пидорленд @{Usernic}!')
-                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+            if usernick is not None:
+                bott.send_message(message.chat.id, f'Добро пожаловать в Пидорленд @{usernick}!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBSDRgnQxwAVY-NmkrPchTYX9i0XyrBgACsQMAAgk7OxNuaRS5G0Mbjh8E')
 
             else:
-                bott.send_message(message.chat.id, f'Добро пожаловать в Пидорленд {Username}!')
-                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+                bott.send_message(message.chat.id, f'Добро пожаловать в Пидорленд {username}!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBSDRgnQxwAVY-NmkrPchTYX9i0XyrBgACsQMAAgk7OxNuaRS5G0Mbjh8E')
 
             with open('users.json', "r") as users_file_re:
                 infor = json.load(users_file_re)
 
-                if Usernic is None:
-                    infor["id"] += User_chat_id,
-                    infor["Users"] += Username,
-                    infor["Score"] += Score,
+                if usernick is None:
+                    infor["id"] += user_chat_id,
+                    infor["Users"] += username,
+                    infor["Score"] += score,
 
                     with open('users.json', "w"):
-
                         with open('users.json', "a") as users_file_a:
                             json.dump(infor, users_file_a)
                             users_file_a.close()
-
                 else:
-                    infor["id"] += User_chat_id,
-                    infor["Users"] += Usernic,
-                    infor["Score"] += Score,
+                    infor["id"] += user_chat_id,
+                    infor["Users"] += usernick,
+                    infor["Score"] += score,
 
                     with open('users.json', "w"):
 
@@ -98,9 +91,8 @@ def reg_pidar(message):
                             users_file_a.close()
     except:
         bott.send_message(traceback.format_exc())
-        print("chel tiiii")
 
-# Chouse pidor of day
+# Choose pidor of day
 @bott.message_handler(commands=['choose_pidr'])
 def choose_pidr(message):
 
@@ -110,46 +102,80 @@ def choose_pidr(message):
         pidr_of_day = infor["Pidr_of_day"]
         today = datetime.datetime.now()
         trigger = today.day - (datetime.datetime.strptime(infor["Today"], "%Y/%m/%d")).day
-        print(trigger)
+
+        if "01-01" in f"{today}":
+            with open('users.json', "r") as users_file_re:
+                infor = json.load(users_file_re)
+                tabel = f"Ими гордится пидорленд:\n"
+            for username in (infor["Users"]):
+                username_index = infor["Users"].index(username)
+                infor["Score"][username_index] = 0
+                with open('users.json', "w"):
+                    with open('users.json', "a") as users_file_a:
+                        json.dump(infor, users_file_a)
+                        users_file_a.close()
+            bott.send_message(message.chat.id, f'{tabel}')
+            bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED9XBiDlmNHt2G-tNHE6pQsgboMKHXIwACtQMAAgk7OxPvqVG89ySCliME')
+
         if trigger >= 1:
 
-            Username_index = infor["Users"].index(rnd_user)
-            print(f"Username_index {Username_index}")
+            username_index = infor["Users"].index(rnd_user)
+            print(f"Username_index {username_index}")
 
-            infor["Score"][Username_index] = infor["Score"][Username_index] + 1
+            infor["Score"][username_index] = infor["Score"][username_index] + 1
             print(infor["Score"])
 
-            bott.send_message(message.chat.id, f'Кто-то сын маминой подруги, а ты @{rnd_user} пидор!!!')
-            bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+            if "12-31" in f"{today}":
+                bott.send_message(message.chat.id, f'Сектор ОЧКО товарща на барабане, это значит, что пидор годы ты @{rnd_user} !!!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED9XRiDl0UIIQAAR1qjKZVMBrzd92cAsQAAq8DAAIJOzsTeE1g2lC8hGMjBA')
+            else:
+                bott.send_message(message.chat.id, f'Кто-то сын маминой подруги, а ты @{rnd_user} пидор!!!')
+                bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOxNgiicHjwSxTeyWXsLcGQtuLzY17QACqQMAAgk7OxNc9U5oe6y0NR8E')
+
             infor["Today"] = f"{today.year}/{today.month}/{today.day}"
             infor["Pidr_of_day"] = rnd_user
+
             with open('users.json', "w"):
                 with open('users.json', "a") as users_file_a:
                     json.dump(infor, users_file_a)
                     users_file_a.close()
         else:
-            bott.send_message(message.chat.id, f'Пидор дня {pidr_of_day}!')
+            if "12-31" in f"{today}":
+                bott.send_message(message.chat.id, f'Пидор года {pidr_of_day}!')
+            else:
+                bott.send_message(message.chat.id, f'Пидор дня {pidr_of_day}!')
         print(f'{rnd_user} gay')
 
-#TODO Add xmas pidor of the year
 #TODO Add phrases when you choose pidr of the day
-#TODO Clear json and create new after New Year
 
-# Stat
+# Statistic
 @bott.message_handler(commands=['pidor_score'])
 def pidor_score(message):
+
     with open('users.json', "r") as users_file_re:
         infor = json.load(users_file_re)
         tabel = f"Ими гордится пидорленд:\n"
     for username in (infor["Users"]):
         score_index = infor["Users"].index(username)
         score = infor["Score"][score_index]
+        if score < 1:
+            hyi = "хуёв"
+        elif score >= 1 and score < 2:
+            hyi = "хуй"
+        elif score >= 2 and score <= 4:
+            hyi = "хуя"
+        elif score > 4 and score < 21:
+            hyi = "хуёв"
+        elif score == 21 or score == 31 or score == 41 or score == 51 or score == 61:
+            hyi = "хуй"
+        elif score == 30 or score == 40 or score == 50 or score == 60:
+            hyi = "хуёв"
+        else:
+            hyi = "хуя"
 
-        tabel += f"{username}: {score} хуй\n"
+        tabel += f"{username}: {score} {hyi}\n"
+
     bott.send_message(message.chat.id, f'{tabel}')
-    print(tabel)
-    print('gay')
-
-#TODO Declension of nouns after numeric
+    bott.send_sticker(message.chat.id, 'CAACAgIAAxkBAAED9XBiDlmNHt2G-tNHE6pQsgboMKHXIwACtQMAAgk7OxPvqVG89ySCliME')
 
 bott.polling(none_stop=True, interval=0)
